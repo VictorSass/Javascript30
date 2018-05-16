6b083a9756d9c2d93e2ecf5e99e4a9091772e480
Victor Sassonia <victor.sassonia@gmail.com>
2018-05-16 23:12:28 +0200

    Added exo 7

--- db/seeds.rb
+++ db/seeds.rb
@@ -532,7 +532,49 @@ searchInput.addEventListener("keyup", displayMatches);
 
   </body>')
 
-d7 = Draft.create!(name: "07 - Array Cardio Day 2", completed: "No")
+d7 = Draft.create!(name: "07 - Array Cardio Day 2", completed: "Yes", intro: "", completion: "<body>
+  <p><em>To understand today`s exercise have a look at the JavaScript Console 💁 and the page sourcecode !</p>
+  <script>
+    // ## Array Cardio Day 2
+    const people = [
+      { name: 'Wes', year: 1988 },
+      { name: 'Kait', year: 1986 },
+      { name: 'Irv', year: 1970 },
+      { name: 'Lux', year: 2015 }
+    ];
+    const comments = [
+      { text: 'Love this!', id: 523423 },
+      { text: 'Super good', id: 823423 },
+      { text: 'You are the best', id: 2039842 },
+      { text: 'Ramen is my fav food ever', id: 123523 },
+      { text: 'Nice Nice Nice!', id: 542328 }
+    ];
+    // Some and Every Checks
+    // Array.prototype.some() // is at least one person 19 or older?
+      const isAdult = people.some(person => {
+        const currentYear = (new Date()).getFullYear();
+        return currentYear - person.year >= 19;
+      })
+      console.log(isAdult);
+    // Array.prototype.every() // is everyone 19 or older?
+      const allAdults = people.every(person => {
+        const currentYear = (new Date()).getFullYear();
+        return currentYear - person.year >= 19;
+      })
+      console.log(allAdults);
+    // Array.prototype.find()
+    // Find is like filter, but instead returns just the one you are looking for
+    // find the comment with the ID of 823423
+      const comment = comments.find(comment => comment.id === 823423);
+      console.log(comment);
+    // Array.prototype.findIndex()
+    // Find the comment with this ID
+    // delete the comment with the ID of 823423
+      const index = comments.findIndex(comment => comment.id === 823423);
+      console.log(index);
+      comments.splice(index, 1);
+  </script>
+</body>")
 d8 = Draft.create!(name: "08 - Fun with HTML5 Canvas", completed: "No")
 d9 = Draft.create!(name: "09 - Dev Tools Domination", completed: "No")
 d10 = Draft.create!(name: "10 - Hold Shift and Check Checkboxes", completed: "No")
