156a0c30b1fbb813eadbedae984ac5f8a49bb227
Victor Sassonia <victor.sassonia@gmail.com>
2018-05-22 17:31:51 +0200

    Added exo 8

--- db/seeds.rb
+++ db/seeds.rb
@@ -575,7 +575,65 @@ d7 = Draft.create!(name: "07 - Array Cardio Day 2", completed: "Yes", intro: "",
       comments.splice(index, 1);
   </script>
 </body>")
-d8 = Draft.create!(name: "08 - Fun with HTML5 Canvas", completed: "No")
+d8 = Draft.create!(name: "08 - Fun with HTML5 Canvas", completed: "Yes", into:"", completion: '<body>
+<canvas id="draw" width="800" height="800"></canvas>
+<script>
+  const canvas = document.querySelector("#draw");
+  const ctx = canvas.getContext("2d");
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  ctx.strokeStyle = "#BADA55";
+  ctx.lineJoin = "round";
+  ctx.lineCap = "round";
+  ctx.lineWidth = 50;
+  ctx.globalCompositeOperation = "overlay";
+
+  let isDrawing = false;
+  let lastX = 0;
+  let lastY = 0;
+  let hue = 0;
+  let direction = true;
+
+  function draw(e) {
+    if (!isDrawing) return;
+    console.log(e);
+    ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+    ctx.beginPath();
+    ctx.moveTo(lastX, lastY);
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.stroke();
+    [lastX, lastY] = [e.offsetX, e.offsetY];
+    hue++;
+    if(hue >= 360) {
+      hue = 0;
+    }
+    if (ctx.lineWidth >= 75 || ctx.lineWidth <= 1) {
+      direction = !direction;
+    }
+    if (direction) {
+    ctx.lineWidth++;
+    } else {
+      ctx.lineWidth--;
+    }
+  }
+
+  canvas.addEventListener("mousedown", (e) => {
+    isDrawing = true;
+    [lastX, lastY] = [e.offsetX, e.offsetY];
+  });
+
+  canvas.addEventListener("mousemove", draw);
+  canvas.addEventListener("mouseup", () => isDrawing = false);
+  canvas.addEventListener("mouseout", () => isDrawing = false);
+</script>
+
+<style>
+  html, body {
+    margin:0;
+  }
+</style>
+</body>')
+
 d9 = Draft.create!(name: "09 - Dev Tools Domination", completed: "No")
 d10 = Draft.create!(name: "10 - Hold Shift and Check Checkboxes", completed: "No")
 d11 = Draft.create!(name: "11 - Custom Video Player", completed: "No")
