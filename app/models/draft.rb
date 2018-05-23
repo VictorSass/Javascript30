001571a20b0ab46441a25b70d5d21db76942f138
Victor Sassonia <victor.sassonia@gmail.com>
2018-05-23 23:42:49 +0200

    Added previous and next, bug on previous

--- app/models/draft.rb
+++ app/models/draft.rb
@@ -1,3 +1,13 @@
 class Draft < ApplicationRecord
   default_scope { order('id ASC') }
+  scope :next, lambda {|id| where("id > ?", id).order("id ASC") }
+  scope :previous, lambda {|id| where("id < ?", id).order("id DESC") }
+
+  def next
+    Draft.next(self.id).first
+  end
+
+  def previous
+    Draft.previous(self.id).first
+  end
 end
