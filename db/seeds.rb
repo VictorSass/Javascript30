a3df38f9a5049a03f4f6a7e963b0014445789493
Victor Sassonia <victor.sassonia@gmail.com>
2018-06-15 01:07:29 +0200

    Minimal change on exo 25

--- db/seeds.rb
+++ db/seeds.rb
@@ -1250,8 +1250,8 @@ const divs = document.querySelectorAll("div");
 const button = document.querySelector("button");
 
 function logText(e) {
-  console.log(this.classList.value);
-  // e.stopPropagation(); // Stop taking all parents
+  console.log(this.classList.value, "No propagation");
+  e.stopPropagation(); // Stop taking all parents/bubbling
 }
 
 divs.forEach(div => div.addEventListener("click", logText, {
